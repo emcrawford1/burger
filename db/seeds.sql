307dcb1f1fc6254f1caf1850c8f43ed8443f7258
mrCrawford <noreply>
2019-04-27 19:54:14 -0500

    Initial database schema and seeds files complete.

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+USE burger_db;
+
+INSERT INTO burgers(burger_name) VALUES ('just straight beef');
+INSERT INTO burgers(burger_name) VALUES ('chicken burger');
+INSERT INTO burgers(burger_name) VALUES ('roast beast');
